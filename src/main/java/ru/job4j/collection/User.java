8b955823252f9aa9a712967d4ac4e07410d106a4
StanislavAerialist <moonlight_stas@mail.ru>
2022-09-12 12:46:28 +0500

    1. Создать модель User [#999]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,12 +1,18 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
-    public User() {
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
     }
 }

ae68ceb7e71b50a2a5f2a67cfe7d460aa6fd8ca7
StanislavAerialist <moonlight_stas@mail.ru>
2022-09-16 13:56:34 +0500

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= 9; j++) {
+                    out.write((i * j + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

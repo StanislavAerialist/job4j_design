432a466f36e850e0cd64c932b3d40c5599f5667a
StanislavAerialist <moonlight_stas@mail.ru>
2022-09-21 12:40:19 +0500

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path file : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(file)));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(file)))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validate(ArgsName args) {
+        Path path = Paths.get(args.get("d"));
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException(String.format("Not exists: %s", path));
+        }
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException(String.format("Not directory: %s", path));
+        }
+        if (!args.get("e").startsWith(".")) {
+            throw new IllegalArgumentException(String.format("Exclusion must start from . : %s", args.get("e")));
+        }
+        if (!args.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException(String.format("Target file must be zip: %s", args.get("o")));
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
+        ArgsName argsName = ArgsName.of(args);
+        zip.validate(argsName);
+        List<Path> paths = Search.search(Paths.get(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
+        zip.packFiles(paths, new File(argsName.get("o")));
+    }
+}

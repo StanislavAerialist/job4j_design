55bd7c509d86b8cc3e3f814c90e90398b826fc7f
StanislavAerialist <moonlight_stas@mail.ru>
2022-10-05 21:17:26 +0500

    1. Схема прав пользователи и роли. [#1093 #348870]

--- SQL/query/InnerJoin.sql
+++ SQL/query/InnerJoin.sql
@@ -18,6 +18,19 @@ insert into furniture(name, weight, colour_id) values ('Table', 12, 1);
 insert into furniture(name, weight, colour_id) values ('Chair', 3, 2);
 insert into furniture(name, weight, colour_id) values ('Dresser', 10, 3);
 
-select pp.name, p.name from colour as p join furniture as pp on pp.colour_id = p.id;
-select pp.name, pp.weight, p.name from colour as p join furniture as pp on pp.colour_id = p.id;
-select pp.weight, p.name from colour as p join furniture as pp on pp.colour_id = p.id;
+select pp.name as "Наименование изделия", p.name as "Название цвета"
+from colour as p
+join furniture as pp
+on pp.colour_id = p.id;
+
+select pp.name as "Наименование изделия", pp.weight as "Вес изделия", p.name as "Название цвета"
+from colour as p
+join furniture as pp
+on pp.colour_id = p.id
+where pp.weight > 5 and pp.weight < 12;
+
+select pp.weight as "Вес изделия", p.name as "Название цвета"
+from colour as p
+join furniture as pp
+on pp.colour_id = p.id
+where p.name like 'O%';

27054730af6c9d5e790f1573fdc4c7dbfc06b439
StanislavAerialist <moonlight_stas@mail.ru>
2022-10-05 20:50:08 +0500

    1. Схема прав пользователи и роли. [#1093 #348870]

--- SQL/createDB/create.sql
+++ SQL/createDB/create.sql
@@ -1,42 +1,42 @@
-create table users(
-id serial primary key,
-    name text,
-    item_id int references item(id)
-);
 create table role(
 id serial primary key,
-    name text,
-    users_id int references users(id)
+    name text
 );
 create table rules(
 id serial primary key,
-    name text,
+    name text
 );
 create table role_rules(
 id serial primary key,
     role_id int references role(id),
     rules_id int references rules(id)
 );
+create table users(
+id serial primary key,
+    name text,
+    role_id int references role(id)
+);
+create table category(
+id serial primary key,
+    name text
+);
+create table state(
+id serial primary key,
+    name text
+);
 create table item(
 id serial primary key,
     name text,
-    comments_id int references comments(id),
-    attachs_id int references attachs(id)
+    users_id int references users(id),
+    catagory_id int references category(id),
+    state_id int references state(id)
 );
 create table comments(
-id serial primary key,
-    name text
-);
-create table attachs(
-id serial primary key,
-    name text
-);
-create table category(
 id serial primary key,
     name text,
     item_id int references item(id)
 );
-create table state(
+create table attachs(
 id serial primary key,
     name text,
     item_id int references item(id)

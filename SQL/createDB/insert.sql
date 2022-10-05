d0c64fc911482d2444bdeaca0372b2ad70cb220c
StanislavAerialist <moonlight_stas@mail.ru>
2022-10-05 22:05:35 +0500

    1. Схема прав пользователи и роли. [#1093 #348870]

--- SQL/createDB/insert.sql
+++ SQL/createDB/insert.sql
@@ -1,8 +1,10 @@
 insert into role(name) values('admin');
 insert into rules(name) values('rule1');
+insert into role_rules(role_id, rules_id) values (1, 1);
+insert into state(name) values('state1');
+insert into category(name) values('category1');
 insert into users(name, role_id) values('Pavel', 1);
 insert into item(name, users_id, category_id, state_id) values('item1', 1, 1, 1);
 insert into comments(name, item_id) values('comment1', 1);
 insert into attachs(name, item_id) values('attachs1', 1);
-insert into category(name) values('category1');
-insert into state(name) values('state1');
+

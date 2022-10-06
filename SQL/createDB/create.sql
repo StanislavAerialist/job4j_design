c8e078ec9b616332e9025a21e2e6be03b1bb9f28
StanislavAerialist <moonlight_stas@mail.ru>
2022-10-06 17:50:15 +0500

    1. Схема прав пользователи и роли. [#1093 #348870]

--- SQL/createDB/create.sql
+++ SQL/createDB/create.sql
@@ -28,7 +28,7 @@ create table item(
 id serial primary key,
     name text,
     users_id int references users(id),
-    catagory_id int references category(id),
+    category_id int references category(id),
     state_id int references state(id)
 );
 create table comments(

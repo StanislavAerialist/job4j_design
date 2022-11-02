703adaa24620bb574e01b93de06adc79884cb95d
StanislavAerialist <moonlight_stas@mail.ru>
2022-11-02 22:47:04 +0500

    2. Найти утечку памяти. [#504882]

--- src/main/java/ru/job4j/gc/leak/UserGenerator.java
+++ src/main/java/ru/job4j/gc/leak/UserGenerator.java
@@ -14,10 +14,10 @@ public class UserGenerator implements Generate {
     public static final String SEPARATOR = " ";
     public static final Integer NEW_USERS = 1000;
 
-    public static List<String> names;
-    public static List<String> surnames;
-    public static List<String> patrons;
-    private static List<User> users = new ArrayList<>();
+    public List<String> names;
+    public List<String> surnames;
+    public List<String> patrons;
+    private List<User> users = new ArrayList<>();
     private Random random;
 
     public UserGenerator(Random random) {
@@ -29,10 +29,12 @@ public class UserGenerator implements Generate {
     public void generate() {
         users.clear();
         for (int i = 0; i < NEW_USERS; i++) {
-            users.add(new User(
-                    surnames.get(random.nextInt(surnames.size())) + SEPARATOR
-                            + names.get(random.nextInt(names.size())) + SEPARATOR
-                            + patrons.get(random.nextInt(patrons.size()))));
+            users.add(new User(String.valueOf(new StringBuilder()
+                    .append(surnames.get(random.nextInt(surnames.size())))
+                    .append(SEPARATOR)
+                    .append(names.get(random.nextInt(names.size())))
+                    .append(SEPARATOR)
+                    .append(patrons.get(random.nextInt(patrons.size()))))));
         }
     }
 
@@ -50,7 +52,7 @@ public class UserGenerator implements Generate {
         return users.get(random.nextInt(users.size()));
     }
 
-    public static List<User> getUsers() {
+    public List<User> getUsers() {
         return users;
     }
 }

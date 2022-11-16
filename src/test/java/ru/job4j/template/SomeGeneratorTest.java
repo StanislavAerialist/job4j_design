71ae749d0728729a21ec6ca069688a8e2fae1aab
StanislavAerialist <moonlight_stas@mail.ru>
2022-11-16 22:48:13 +0500

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/SomeGeneratorTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SomeGeneratorTest {
+
+    @Test
+    void whenProduceSuccess() {
+        SomeGenerator gen = new SomeGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        String rsl = "I am a Petr Arsentev, Who are you? ";
+        assertThat(gen.produce(template, args)).isEqualTo(rsl);
+    }
+
+    @Test
+    void whenProduceThenInvalidKey() {
+        SomeGenerator gen = new SomeGenerator();
+        String template = "I am a ${lastname}, Who are ${subject}? ";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        assertThrows(IllegalArgumentException.class, () -> {
+            gen.produce(template, args);
+        });
+    }
+
+    @Test
+    void whenProduceThenInvalidTemplate() {
+        SomeGenerator gen = new SomeGenerator();
+        String template = "I am a ${name}, ${lastname}, Who are ${subject}? ";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        assertThrows(IllegalArgumentException.class, () -> {
+            gen.produce(template, args);
+        });
+    }
+}

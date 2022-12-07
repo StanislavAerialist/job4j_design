f853e6ea5abfdbafd3eda89286f836d3ee5b171d
StanislavAerialist <moonlight_stas@mail.ru>
2022-12-07 19:34:52 +0500

    1. Хранилище продуктов [#852 #367574]

--- src/main/java/ru/job4j/ood/lcp/storage/Shop.java
+++ src/main/java/ru/job4j/ood/lcp/storage/Shop.java
@@ -3,10 +3,10 @@ package ru.job4j.ood.lcp.storage;
 import java.util.Calendar;
 
 public class Shop extends AbstractStore {
-    private final ExpirationCalculator<Calendar> expirationCalculator;
     public static final double WAREHOUSE = 25;
     private static final double DISCOUNT = 75;
     public static final double TRASH = 100;
+    private final ExpirationCalculator<Calendar> expirationCalculator;
 
     public Shop(ExpirationCalculator<Calendar> expirationCalculator) {
         this.expirationCalculator = expirationCalculator;

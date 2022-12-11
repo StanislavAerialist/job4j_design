a98485fa77bbe6b5b014680b36344f619144480e
StanislavAerialist <moonlight_stas@mail.ru>
2022-12-11 19:30:16 +0500

    0. Принцип инверсии зависимостей [#4917 #369581]

--- src/main/java/ru/job4j/ood/dip/StringProcessor.java
+++ src/main/java/ru/job4j/ood/dip/StringProcessor.java
@@ -12,7 +12,6 @@ public class StringProcessor {
         this.stringReader = stringReader;
         this.stringWriter = stringWriter;
     }
-
     public void printString() {
         stringWriter.write(stringReader.toString());
     }

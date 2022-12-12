0b84e0def3bf616e98efe98d6165db9cf0efb090
StanislavAerialist <moonlight_stas@mail.ru>
2022-12-12 18:52:45 +0500

    1. Создать меню. [#4748 #369567]

--- src/main/java/ru/job4j/ood/isp/menu/TreePrint.java
+++ src/main/java/ru/job4j/ood/isp/menu/TreePrint.java
@@ -6,18 +6,16 @@ import java.util.regex.Pattern;
 
 public class TreePrint implements MenuPrinter {
 
+    public static final String DEC = "----";
+
     @Override
     public void print(Menu menu) {
         Iterator<Menu.MenuItemInfo> iterator = menu.iterator();
-        while (iterator.hasNext()) {
-            Menu.MenuItemInfo next = iterator.next();
-            StringBuilder s = new StringBuilder();
-            Pattern p = Pattern.compile("\\.");
-            Matcher m = p.matcher(next.getNumber());
-            while (m.find()) {
-                s.append("----");
-            }
-            System.out.printf(s + next.getNumber() + next.getName() + "\n");
+        for (Menu.MenuItemInfo item : menu) {
+            int count = item.getNumber().split("\\.").length - 1;
+            System.out.println(DEC.repeat(count)
+                    + item.getNumber()
+                    + item.getName());
         }
     }
 }
